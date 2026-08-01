8d0e613f9369f36d0c85a3aecab947f1b9e4495c
Kenichi Wakabayashi <kenichi@crssrds.jp>
2026-08-02 06:45:14 +0900

    説明文変更、SNSリンク間違い修正

--- starpod.config.ts
+++ starpod.config.ts
@@ -10,7 +10,7 @@ export default defineStarpodConfig({
   itunesImage: '/images/radio-3000.png',
   // 配信形式: 各回独立なら 'episodic'、連続ものなら 'serial'。
   type: 'episodic',
-  blurb: 'ITとかAIとか学びについてその時の気分で話します',
+  blurb: 'ITとかAIとか学びについて徒然なるままに話します',
   description:
     'ITとかAIとか学びとか、若林の関心のあること、話したいことを徒然なるままに話します',
   // 番組の連絡先メール。RSSの itunes:owner / managingEditor に出力される。
@@ -26,7 +26,7 @@ export default defineStarpodConfig({
       // TODO: src/images/people/ にご自身の写真を置いてファイル名を指定してください
       img: 'me.jpg',
       github: 'https://github.com/kwaka1208',
-      twitter: 'https://x.com/waka1208',
+      twitter: 'https://x.com/kwaka1208',
       website: 'https://crssrds.jp'
     },
   ],
@@ -34,9 +34,7 @@ export default defineStarpodConfig({
     apple:
       'https://podcasts.apple.com/us/podcast/crossradio/id6797005458',
     appleIdNumber: '6797005458',
-    // TODO: Amazon Music の番組ページURLに差し替える（コメントを外すと Listen に表示）
-    // 実URL例: https://music.amazon.com/podcasts/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
-    // amazon: 'https://music.amazon.com/podcasts/xxxxxxxx-xxxx-xxxx',
+    amazon: 'https://podcasters.amazon.com/podcasts/7516ae41-433e-4049-b082-ec701b44f284',
     // overcast: 'https://overcast.fm/itunes1552776603',
     // pocketCasts: 'https://pca.st/bezzctzj',
     spotify: 'https://open.spotify.com/show/033ZEW5vFEKgEGyv6zRwuK',
